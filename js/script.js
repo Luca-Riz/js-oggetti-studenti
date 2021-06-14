352a5c90f68f939af0246f64ccfb55630b829381
Luca-Riz <77754414+Luca-Riz@users.noreply.github.com>
2021-06-14 15:22:33 +0200

    add ciclo su tutti studenti e stampa nome cognome

--- js/script.js
+++ js/script.js
@@ -21,9 +21,25 @@ for (var k in studente){
 // Creare un array di oggetti di studenti.
 arrayStudenti = [studente];
 
-console.log(arrayStudenti);
+ studente = {
+    nome: 'Peperone',
+    cognome: 'Paperon De',
+    eta: 80
+};
+arrayStudenti.push(studente);
+
+studente = {
+    nome: 'Pippo',
+    cognome: 'Nonricordo',
+    eta: 35
+};
+arrayStudenti.push(studente);
 
 // Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
 
+for (var i=0; i<arrayStudenti.length; i++){
+    console.log(arrayStudenti[i].nome);
+    console.log(arrayStudenti[i].cognome);
+}
 
 // Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
